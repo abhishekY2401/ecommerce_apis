307b94f035064117a5d3745a80b06fcd69f792ca
Abhishek Yadav <abhiyd2401@gmail.com>
2023-07-10 00:19:42 +0530

    loaded env variables

--- db.py
+++ db.py
@@ -1,9 +1,18 @@
 from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 
 def connect_to_database():
+
+    username = os.environ.get("mongo_username")
+    password = os.environ.get("mongo_password")
+
     # connection url
-    CONNECTION_URL = "mongodb+srv://yadavabhishek6064:Abhiraj123@cluster0.nr1dxef.mongodb.net/?retryWrites=true&w=majority"
+    CONNECTION_URL = "mongodb+srv://{}:{}@cluster0.nr1dxef.mongodb.net/?retryWrites=true&w=majority".format(
+        username, password)
 
     # connect using MongoClient
     client = MongoClient(CONNECTION_URL)
